3dfc6fa7d4dd0b53078c8e714ae9221af11e7a6e
Bryce <bryce@VICDEVLAPT003.readify>
2019-07-27 22:42:37 +1000

    i dunno

--- components/categoryCard.js
+++ components/categoryCard.js
@@ -1,24 +1,35 @@
+//Import Styling
+//import { makeStyles } from '@material-ui/core/styles';
+//Import Components
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
 
+//const useStyles = makeStyles(theme => ({
+//}));
 
-let CategoryCard = () => (
 
-  <Card>
-    <CardActionArea>
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          Physics
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          The study of relativity, and quantum mechanics and other bullshit
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
+let CategoryCard = () => {
 
-)
+  //const classes = useStyles();
+
+  return(
+    <div>
+      <Card>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              Physics
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              The study of relativity and quantum mechanics and other bullshit
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  )
+}
 
 export default CategoryCard;
