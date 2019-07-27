3ed1688c1a71abb2f3675e73b5709b5b9f4906c8
Bryce <bryce@VICDEVLAPT003.readify>
2019-07-27 16:51:02 +1000

    its probably fine

---
+++ components/categoryCard.js
@@ -0,0 +1,14 @@
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+let CategoryCard = () => (
+
+  <Card color="secondary">
+    Wow
+  </Card>
+
+)
+
+export default CategoryCard;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import CategoryCard from '../components/CategoryCard';
 
 let index = () => (
 
-    <h1>Hello World</h1>
-    
+    <div>
+      <h1>Hello World</h1>
+      <CategoryCard />
+    </div>
+
 )
 
 
