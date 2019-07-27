b2a8f3a1cb36f7c34d97283021c4b21671d64a1a
Bryce <bryce@VICDEVLAPT003.readify>
2019-07-27 17:55:36 +1000

    whatever man

--- components/categoryCard.js
+++ components/categoryCard.js
@@ -1,11 +1,12 @@
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Icon from '@material-ui/core/Icon';
 
 
 let CategoryCard = () => (
 
-  <Card color="secondary">
+  <Card>
     Wow
   </Card>
 
